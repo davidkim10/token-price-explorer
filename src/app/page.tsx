0380a00b1d1489d0f5738da810cdef37443ee27c
David Kim <davidykim10@gmail.com>
2025-05-22 02:51:50 -0400

    add token card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { TokenCard } from "@/components/TokenCard";
 
 export default function Home() {
   return (
-    <div className="flex flex-grow items-center justify-center h-full p-2">
+    <main className="flex flex-grow items-center justify-center h-full p-2">
       <TokenCard />
-    </div>
+    </main>
   );
 }
