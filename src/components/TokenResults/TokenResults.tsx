95cd0753799e6dda9e604a4852baf01b2943ccee
David Kim <davidykim10@gmail.com>
2025-05-22 11:56:59 -0400

    convert tooltip to popover to fix mobile trigger issue

--- src/components/TokenResults/TokenResults.tsx
+++ src/components/TokenResults/TokenResults.tsx
@@ -1,13 +1,12 @@
 import { Info } from "lucide-react";
 import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { formatNumber } from "@/lib/utils";
-import { TokenResultsList } from "./TokenResultsList";
 import type { TokenPrice } from "@/types";
+import { TokenResultsList } from "./TokenResultsList";
 
 interface TokenResultsProps {
   sourceToken: TokenPrice;
@@ -25,32 +24,30 @@ export const TokenResults = ({
     <div className="rounded-lg bg-stone-100 dark:bg-slate-800/20 p-3 text-sm font-medium">
       <div className="flex justify-between items-center mb-4 text-gray-400 dark:text-gray-600">
         <h2 className="text-sm text-gray-400 font-medium">Token Breakdown</h2>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger>
-              <Info size={16} className="text-gray-400" />
-            </TooltipTrigger>
-            <TooltipContent>
-              <h3 className="text-sm font-semibold mb-1 text-gray-400 dark:text-gray-600">
-                Unit Price
-              </h3>
+        <Popover>
+          <PopoverTrigger>
+            <Info size={16} className="text-gray-400" />
+          </PopoverTrigger>
+          <PopoverContent className="max-w-50">
+            <h3 className="text-sm font-semibold mb-2 text-gray-400 dark:text-gray-600">
+              Unit Price
+            </h3>
 
-              {tokens.map((token) => (
-                <div
-                  className="text-xs flex justify-between gap-2 text-gray-400 dark:text-gray-600"
-                  key={token.symbol}
-                >
-                  <span className="text-xs font-semibold mr-2">
-                    {token.symbol}
-                  </span>
-                  <span className="text-xs text-ellipsis overflow-hidden">
-                    {formatNumber(token.price)}
-                  </span>
-                </div>
-              ))}
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+            {tokens.map((token) => (
+              <div
+                className="text-xs flex justify-between gap-2 text-gray-400 dark:text-gray-600"
+                key={token.symbol}
+              >
+                <span className="text-xs font-semibold mr-2">
+                  {token.symbol}
+                </span>
+                <span className="text-xs text-ellipsis overflow-hidden">
+                  {formatNumber(token.price)}
+                </span>
+              </div>
+            ))}
+          </PopoverContent>
+        </Popover>
       </div>
 
       <TokenResultsList
